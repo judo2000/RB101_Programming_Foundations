62f7ff2756162efcc245131f7fe0e1e4fc274921
Stephen Moore <judo2000@comcast.net>
2020-03-20 15:12:44 -0600

    Update calculator_bonus and calculator_messages files

--- lesson_2/calculator_bonus.rb
+++ lesson_2/calculator_bonus.rb
@@ -49,16 +49,52 @@ def clear_screen
   system("clear") || system("cls")
 end
 
+def retrieve_operator(lang)
+  prompt(messages('operator_prompt', lang))
+  gets.chomp
+end
+
+def zero_division(op, num)
+  op == '4' && num == '0'
+end
+
 def operation_to_message(op, lang)
   case op
   when '1'
-    messages('add', lang)
+    messages('adding', lang)
   when '2'
     messages('subtract', lang)
   when '3'
     messages('multipling', lang)
   when '4'
     messages('divide', lang)
+  end
+end
+
+def calculate_result(operator, number1, number2, lang)
+  result = case operator
+           when '1'
+             number1.to_f + number2.to_f
+           when '2'
+             number1.to_f - number2.to_f
+           when '3'
+             number1.to_f * number2.to_f
+           when '4'
+             number1.to_f / number2.to_f
+           end
+  prompt(messages('result', lang) + " #{result}")
+end
+
+def continue(lang)
+  answer = ''
+  loop do
+    prompt(messages('continue', lang))
+    answer = gets.chomp.downcase()
+    if answer == "y" || answer == "n"
+      break
+    else
+      prompt(messages('continue_error'))
+    end
   end
 end
 
@@ -100,35 +136,27 @@ loop do
       prompt(messages('not_valid_num', lang))
     end
   end
-
-  prompt(messages('operator_prompt', lang))
+  prompt(messages('another_calculation', lang))
 
   operator = ''
   loop do
-    operator = gets.chomp
-
-    if %w(1 2 3 4).include?(operator)
+    operator = retrieve_operator(lang)
+    if zero_division(operator, number2)
+      prompt(messages('zero_division', lang))
+      next
+    elsif %w(1 2 3 4).include?(operator)
+      operation_to_message(operator, lang)
       break
-    else
-      prompt(messages('operator_error', lang))
     end
+    prompt(messages('operator_error', lang))
   end
-  prompt("#{operation_to_message(operator, lang)} the two numbers...")
 
-  result = case operator
-          when '1'
-            number1.to_f + number2.to_f
-          when '2'
-            number1.to_f - number2.to_f
-          when '3'
-            number1.to_f * number2.to_f
-          when '4'
-            number1.to_f / number2.to_f
-          end
-  prompt(messages('result', lang) + " #{result}")
-  prompt(messages('another_calculation', lang))
-  answer = gets.chomp
-  break unless answer.downcase().start_with?('y')
-  clear_screen
-  prompt(messages('good_bye', lang))
+  calculate_result(operator, number1, number2, lang)
+
+  if continue(lang) == "y"
+    clear_screen
+  else
+    prompt(messages('good_bye', lang))
+    break
+  end
 end
