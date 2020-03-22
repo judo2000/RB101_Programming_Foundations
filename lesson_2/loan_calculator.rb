4db6695d282882dd818b7f44df099c3b3540e570
Stephen Moore <judo2000@comcast.net>
2020-03-22 15:08:25 -0600

    update loan calculator and loan calc messages

--- lesson_2/loan_calculator.rb
+++ lesson_2/loan_calculator.rb
@@ -1,51 +1,63 @@
+require "yaml"
+MESSAGES = YAML.load_file("loan_calc_messages.yml")
+
 def prompt(message)
   puts "=> #{message}"
 end
 
-loop do
-  prompt("Welcome to Loan Calculator!")
-  prompt("---------------------------")
+def messages(message)
+  MESSAGES[message]
+end
 
-  prompt("What is the loan amount?")
+def get_loan_amount
+  prompt(messages('loan_amount'))
 
   amount = ''
   loop do
     amount = gets.chomp
 
-    if amount.empty? || amount.to_f < 0
-      prompt("Loan amount must be a positive number.")
+    if amount.empty? || amount.to_f < 0 || amount.to_f <= 0
+      prompt(messages('loan_amount_error'))
     else
       break
     end
   end
+  return amount
+end
 
-  prompt("What is the interest rate?")
-  prompt("(Example: 5 for 5% or 2.5 for 2.5%)")
+def get_interest_rate
+  prompt(messages('interest_rate'))
 
   interest_rate = ''
   loop do
     interest_rate = gets.chomp
 
-    if interest_rate.empty? || interest_rate.to_f < 0
-      prompt("Please enter a positive number.")
+    if interest_rate.empty? || interest_rate.to_f <= 0 
+      prompt(messages('interest_rate_error'))
     else
       break
     end
   end
+  return interest_rate
+end
 
-  prompt("What is the loan duration (in years)?")
+def get_loan_duration
+  prompt(messages('loan_duration'))
 
   years = ''
   loop do
     years = gets.chomp
 
-    if years.empty? || years.to_i < 0
-      prompt("Enter a valid number.")
+    if years.empty? || years.to_i <= 0
+      prompt(messages('loan_duration_error'))
     else
       break
     end
   end
+  return years
+end
 
+def get_monthly_payment(amount, interest_rate, years)
   annual_interest_rate = interest_rate.to_f / 100
   monthly_interest_rate = annual_interest_rate / 12
   months = years.to_i * 12
@@ -53,14 +65,46 @@ loop do
   monthly_payment = amount.to_f *
                     (monthly_interest_rate /
                     (1 - (1 + monthly_interest_rate)**(-months)))
+end
+
+def clear_screen
+  system("clear") || system("cls")
+end
+
+def continue
+  answer = ''
+  loop do
+    prompt(messages('continue'))
+    answer = gets.chomp.downcase()
+    if answer == "y" || answer == "n"
+      break
+    else
+      prompt(messages('continue_error'))
+    end
+  end
+  return answer
+end
+
+clear_screen
+loop do
+  prompt(messages('welcome'))
+  prompt(messages('dash_line'))
+
+  amount = get_loan_amount
+  puts amount
+  interest_rate = get_interest_rate
+  
+  years = get_loan_duration
+
+  monthly_payment = get_monthly_payment(amount, interest_rate, years)
 
   prompt("Your monthly payment is: $#{format('%02.2f', monthly_payment)}")
 
-  prompt("another Calculation?")
-  answer = gets.chomp
-
-  break unless answer.downcase.start_with?('y')
+  if continue == "y"
+    clear_screen
+  else
+    break
+  end
 end
-
-prompt("Thank you for using the Loan Calculator!")
-prompt("Good bye!")
+prompt(messages('thank_you'))
+prompt(messages('good_bye'))
